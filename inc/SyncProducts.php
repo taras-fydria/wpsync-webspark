b2961cabf0066658d7ed47190be41e1bf5b39990
taras <taras.fydria@gmail.com>
2023-06-05 20:11:02 +0300

    fix image upload

--- inc/SyncProducts.php
+++ inc/SyncProducts.php
@@ -10,7 +10,7 @@ class SyncProducts extends Singleton
 {
     const API_URI = 'https://wp.webspark.dev/wp-api/products';
 
-    public static function sync_products(): void
+    public static function sync_products(): mixed
     {
         $existing_skus = array();
 
@@ -32,54 +32,60 @@ class SyncProducts extends Singleton
 
             $proceed_products_id = [];
 
-            if ($response_code === 200) {
-                $body_json = wp_remote_retrieve_body($response);
-                $data = json_decode($body_json, true);
-                $products = !empty($data['data']) ? array_map([ProductInput::class, 'build_item'], $data['data']) : [];
-
-                if (!empty($products)) {
-                    /** Process each product from the API
-                     * @var $product ProductInput
-                     */
-
-
-                    $array_chunks = array_chunk($products, 100);
-                    foreach ($array_chunks as $chunk) {
-
-                        foreach ($chunk as $product) {
-
-                            $product_index = array_search($product->sku, $existing_skus);
-                            if (gettype($product_index) === 'integer' && $product_index >= 0) {
-                                // Create a new product
-                                $product_id = $existing_products_id[$product_index];
-                                $proceed_products_id[] = self::update_product($product_id, $product);
-                            } else {
-                                // Update the existing product
-                                $proceed_products_id[] = self::create_product($product);
-                            }
-                        }
-                    }
-
-                    //Delete products  which don't was in response
-                    foreach ($existing_products_id as $existing_id) {
-                        if (!in_array($existing_products_id, $proceed_products_id)) {
-                            $wc_product = wc_get_product($existing_id);
-                            if ($wc_product) $wc_product->delete(true);
-                        }
-                    }
-
-                }
-            } else {
-                // Handle the response code error
+            if ($response_code !== 200) {
                 error_log('Error syncing products. Response code: ' . $response_code);
+                return __('Something Wrong');
             }
-        } catch (Exception $e) {
+
+            $body_json = wp_remote_retrieve_body($response);
+            $data = json_decode($body_json, true);
+            $products = !empty($data['data']) ? array_map([ProductInput::class, 'build_item'], $data['data']) : [];
+
+            if (empty($products)) {
+                return __('Something Wrong');
+            }
+            /** Process each product from the API
+             * @var $product ProductInput
+             */
+
+            $array_chunks = array_chunk($products, 50);
+
+            foreach ($array_chunks as $chunk) {
+
+                foreach ($chunk as $product) {
+
+                    $product_index = array_search($product->sku, $existing_skus);
+                    if (gettype($product_index) === 'integer' && $product_index >= 0) {
+                        // Create a new product
+                        $product_id = $existing_products_id[$product_index];
+                        $proceed_products_id[] = self::update_product($product_id, $product);
+                    } else {
+                        // Update the existing product
+                        $proceed_products_id[] = self::create_product($product);
+                    }
+                }
+            }
+
+            //Delete products  which don't was in response
+            foreach ($existing_products_id as $existing_id) {
+                if (!in_array($existing_products_id, $proceed_products_id)) {
+                    $wc_product = wc_get_product($existing_id);
+                    if ($wc_product) $wc_product->delete(true);
+                }
+            }
+
+        } catch
+        (Exception $e) {
             // Handle the error
             error_log('Error syncing products: ' . $e->getMessage());
         }
 
         // Delete unavailable products
         self::delete_unavailable_products($existing_skus);
+
+        return [
+
+        ];
     }
 
     private static function get_existing_products(): array
@@ -135,11 +141,8 @@ class SyncProducts extends Singleton
         return $wc_product->save();
     }
 
-    static function save_and_attach_image_by_url(int $post_id, string $image_url): int | bool
+    static function save_and_attach_image_by_url(int $post_id, string $image_url): int|bool
     {
-        require_once ABSPATH . 'wp-admin/includes/image.php';
-        require_once ABSPATH . 'wp-admin/includes/file.php';
-        require_once ABSPATH . 'wp-admin/includes/media.php';
 
         $thumbnail_id = get_post_thumbnail_id($post_id);
         if ($thumbnail_id && get_post_meta($thumbnail_id, 'image_url') === $image_url) {
@@ -150,10 +153,52 @@ class SyncProducts extends Singleton
             wp_delete_attachment($thumbnail_id, true);
         }
 
-        // Download the image from the URL
-        $media_id = media_sideload_image($image_url, $post_id);
-        add_post_meta($post_id, 'image_url', $image_url);
+        require_once(ABSPATH . "/wp-load.php");
+        require_once(ABSPATH . "/wp-admin/includes/image.php");
+        require_once(ABSPATH . "/wp-admin/includes/file.php");
+        require_once(ABSPATH . "/wp-admin/includes/media.php");
+        $tmp = download_url($image_url);
+        if (is_wp_error($tmp)) return false;
 
+        $filename = pathinfo($image_url, PATHINFO_FILENAME);
+        $extension = pathinfo($image_url, PATHINFO_EXTENSION);
+
+        // An extension is required or else WordPress will reject the upload
+        if (!$extension) {
+            // Look up mime type, example: "/photo.png" -> "image/png"
+            $mime = mime_content_type($tmp);
+            $mime = is_string($mime) ? sanitize_mime_type($mime) : false;
+
+            // Only allow certain mime types because mime types do not always end in a valid extension (see the .doc example below)
+            $mime_extensions = array(
+                // mime_type         => extension (no period)
+                'text/plain' => 'txt',
+                'text/csv' => 'csv',
+                'application/msword' => 'doc',
+                'image/jpg' => 'jpg',
+                'image/jpeg' => 'jpeg',
+                'image/gif' => 'gif',
+                'image/png' => 'png',
+                'video/mp4' => 'mp4',
+            );
+
+            if (isset($mime_extensions[$mime])) {
+                // Use the mapped extension
+                $extension = $mime_extensions[$mime];
+            } else {
+                // Could not identify extension
+                unlink($tmp);
+                return false;
+            }
+        }
+        $args = array(
+            'name' => "$filename.$extension",
+            'tmp_name' => $tmp,
+        );
+        // Download the image from the URL
+        $media_id = media_handle_sideload($args, 0);
+        add_post_meta($post_id, 'image_url', $image_url);
+        unlink($tmp);
         // If the image was successfully attached, set it as the post thumbnail
         if (!is_wp_error($media_id)) {
             return set_post_thumbnail($post_id, $media_id);
